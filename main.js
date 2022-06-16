718d7c48b7df48188e88c31c013738c4e43da5d8
CalmDeveloper <demir445@gmail.com>
2022-06-16 18:31:56 +0300

    14

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 const app = angular.module('app',[]);
 
+app.controller('myCtrl', function ($scope) {
+$scope.posts=[
+    {name:'post1'},
+    {name:'post2'},
+    {name:'post3'},
+    {name:'post4'},
+    {name:'post5'},
+]
+   $scope.getPosts =()=>{
+    return $scope.posts
+   }
+})
+
+app.directive('myDir',function () {
+    return {
+        scope: false,
+        template: '<div ng-repeat="post in getPosts()">{{post.name}}</div>',
+        link: function (scope,element,attrs) {
+
+        }
+    }
+})
