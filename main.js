3b87e70ea9b33c69bfbb374691bff2272996cc88
CalmDeveloper <demir445@gmail.com>
2022-06-16 13:54:53 +0300

    7 do not work

---
+++ main.js
@@ -0,0 +1,14 @@
+const app = angular.module('app',[])
+app.directive('foo', function () {
+    return {
+        link: function (scope, element, attrs) {
+            element.on('click', function () {
+                if (element.text() === 'foo') {
+                    element.text('bar');
+                } else {
+                    element.text('foo');
+                }
+            });
+        }
+    };
+});
