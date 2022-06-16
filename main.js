75b22ebf0ade38d9461eef6d54528635cbd6108e
CalmDeveloper <demir445@gmail.com>
2022-06-16 17:19:26 +0300

    master

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
 const app = angular.module('app',[]);
 
-app.directive('myDir', function () {
-    const bookmarks = [
-        {name: 'nodeJs'},
-        {name: 'Html'},
-        {name: 'css'}
-    ]
-    return {
-        restrict: 'E',
-        templateUrl: 'bookmarks.html',
-        link: function (scope, element, attrs) {
-            scope.bookmarks = bookmarks
-        }
-    }
-})
