6dfc68ad77712cc955bde984404508968866b280
CalmDeveloper <demir445@gmail.com>
2022-06-17 12:42:28 +0300

    15

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 const app = angular.module('app',[]);
 
+app.controller('myCtrl',function ($scope) {
+    $scope.name = 'Taras'
+    $scope.reverse=()=>{
+$scope.name = $scope.name.split('').reverse().join('')
+    }
+
+})
+
+app.directive('myDir',function () {
+    return {
+        scope:{
+            name:'@',
+            // name:'=',
+            reverse: '&'
+        },
+        template:'<div>{{name}}</div><div><input type="text" ng-model="name"> <button ng-click="reverse()">changeDirname</button></div>',
+        link: function () {
+
+        }
+    }
+})
