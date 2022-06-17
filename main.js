95ec02cbc7d8d6e8fda14b28c6f45d6a8bde76f3
CalmDeveloper <demir445@gmail.com>
2022-06-17 15:32:19 +0300

    18

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 const app = angular.module('app',[]);
 
+
+app.controller('mainCtrl', function($http, $scope) {
+    $http({
+        method: 'get',
+        url: 'https://jsonplaceholder.typicode.com/users'
+    }).then(function (response) {
+        $scope.users= response.data;
+    },function (error){
+        console.log(error, 'can not get data.');
+    });
+
+    $scope.getDetails =(id)=> {
+       let userId = id.id
+        $http({
+        method: 'get',
+        url: `https://jsonplaceholder.typicode.com/users/${userId}`
+    }).then(function (response) {
+      return   $scope.userDetails= response.data;
+    },function (error){
+        console.log(error, 'can not get data.');
+    });}
+});
