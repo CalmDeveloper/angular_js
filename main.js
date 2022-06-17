b72739d21f92f45a063a045471540b84039cc8e5
CalmDeveloper <demir445@gmail.com>
2022-06-17 11:02:45 +0300

    15

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 const app = angular.module('app',[]);
 
+app.controller('myCtrl',function ($scope) {
+    $scope.name = 'Taras'
+})
+
+app.directive('myDir',function () {
+    return {
+        scope:true,
+        template: '<div>my name is {{name}} <input type="text" ng-model="name"></div>',
+        link: function () {
+
+        }
+    }
+})
