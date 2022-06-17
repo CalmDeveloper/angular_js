b21c66a842c5834e1e5bb13ab511a2ed421fbf60
CalmDeveloper <demir445@gmail.com>
2022-06-17 20:19:55 +0300

    18

--- main.js
+++ main.js
@@ -1,24 +1,76 @@
-const app = angular.module('app',[]);
+const app = angular.module('app', []);
 
 
-app.controller('mainCtrl', function($http, $scope) {
+app.controller('carsCtrl', function ($http, $scope) {
+    $scope.isUpdating=null
+    $scope.isUpdatingId=null
     $http({
         method: 'get',
-        url: 'https://jsonplaceholder.typicode.com/users'
+        url: 'http://owu.linkpc.net/api/v2/cars'
     }).then(function (response) {
-        $scope.users= response.data;
-    },function (error){
+        $scope.cars = response.data;
+    }, function (error) {
         console.log(error, 'can not get data.');
     });
 
-    $scope.getDetails =(id)=> {
-       let userId = id.id
+    $scope.createCar = (car) => {
+        const {model, year, price} = car
+
+        const  updCar =  {id:$scope.isUpdatingId,model:model,year: year,price: price}
+
+        if ($scope.isUpdating) {
+            $http({
+                method: 'put',
+                url: `http://owu.linkpc.net/api/v2/cars/${updCar.id}`,
+                data: updCar
+            }).then(function (response) {
+                const index = $scope.cars.findIndex(value => updCar.id === value.id)
+                $scope.cars.splice(index,1)
+                $scope.cars.push(updCar)
+
+            }, function (error) {
+                console.log(error, 'can not get data.');
+            });
+            $scope.isUpdating=false
+            $scope.isUpdatingId=null
+        } else {
+            $http({
+                method: 'post',
+                url: `http://owu.linkpc.net/api/v2/cars`,
+                data: car
+            }).then(function (response) {
+                $scope.cars.push(car)
+            }, function (error) {
+                console.log(error, 'can not get data.');
+            });
+        }
+        $scope.model = null;
+        $scope.price = null;
+        $scope.year = null;
+    }
+
+    $scope.updateCar = (car) => {
+        $scope.isUpdating=true
+        $scope.isUpdatingId=car.id
+        $scope.model = car.model;
+        $scope.price = car.price;
+        $scope.year = car.year;
+    }
+
+    $scope.delCar = (car) => {
+
         $http({
-        method: 'get',
-        url: `https://jsonplaceholder.typicode.com/users/${userId}`
-    }).then(function (response) {
-      return   $scope.userDetails= response.data;
-    },function (error){
-        console.log(error, 'can not get data.');
-    });}
+            method: 'delete',
+            url: `http://owu.linkpc.net/api/v2/cars/${car.id}`
+        }).then(function (response) {
+            const index = $scope.cars.findIndex(value => car.id === value.id)
+            $scope.cars.splice(index, 1)
+        }, function (error) {
+            console.log(error, 'can not get data.');
+        });
+        $scope.model = null;
+        $scope.price = null;
+        $scope.year = null;
+    }
+
 });
